175d90b80eb84727c57ddd90c68e25e794b0df0e
evasquez11 <ethan.f.vasquez-1@ou.edu>
2023-11-14 21:32:05 -0600

    messages cycle through in sensors 1 and 2

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Function to simulate data change for each sensor
-    function simulateDataChange(sensorNumber) {
-        const sensorPower = document.querySelector(`#sensor${sensorNumber} .power`);
-        const statusBar = document.querySelector(`#sensor${sensorNumber} .status-bar`);
-        
-        // Random power value between 0 and 9999 kW
-        sensorPower.textContent = `${Math.floor(Math.random() * 10000)} kW`;
-        // Random status bar height
-        statusBar.style.height = `${Math.floor(Math.random() * 100)}%`;
+    // Messages for the status of each sensor
+    const sensor1Messages = [
+        "No Package Detected - Sensor Idle",
+        "Awaiting Package Arrival",
+        "Package Detected",
+        "Package Expected But Not Detected - Check System"
+    ];
+
+    const sensor2Messages = [
+        "Actuator Inactive - Ready for Engagement",
+        "Actuator Engaged - Operation in Progress",
+        "Error: Actuator Status Unclear"
+    ];
+
+    let sensor1Index = 0;
+    let sensor2Index = 0;
+
+    // Function to update the status message for a sensor
+    function updateStatusMessage(sensorElement, messages, index) {
+        const statusDisplay = sensorElement.querySelector('.status-message');
+        statusDisplay.textContent = messages[index];
     }
 
-    // Simulate data change for all sensors every second
+    // Function to simulate data change for each sensor
+    function simulateDataChange(sensorNumber) {
+        const sensorElement = document.querySelector(`#sensor${sensorNumber}`);
+        const powerDisplay = sensorElement.querySelector('.power');
+
+        // Update power value for sensors 3 to 5
+        if (sensorNumber >= 3) {
+            powerDisplay.textContent = `${Math.floor(Math.random() * 10000)} kW`;
+        }
+    }
+
+    // Update status messages for sensors 1 and 2 at a different interval
     setInterval(() => {
-        for (let i = 1; i <= 5; i++) { // Assuming you have 5 sensors
+        updateStatusMessage(document.querySelector('#sensor1'), sensor1Messages, sensor1Index);
+        updateStatusMessage(document.querySelector('#sensor2'), sensor2Messages, sensor2Index);
+
+        sensor1Index = (sensor1Index + 1) % sensor1Messages.length;
+        sensor2Index = (sensor2Index + 1) % sensor2Messages.length;
+    }, 3000); // Update every 3 seconds for demonstration
+
+    // Simulate data change for sensors 3 to 5
+    setInterval(() => {
+        for (let i = 3; i <= 5; i++) {
             simulateDataChange(i);
         }
-    }, 1000);
+    }, 1000); // Update every second for demonstration
 
     // Handle switch toggle for all sensors
     const switches = document.querySelectorAll('.switch input');

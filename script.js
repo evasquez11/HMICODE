a79ee2ad01810971e4214a36346f3a26d4a3099e
evasquez11 <ethan.f.vasquez-1@ou.edu>
2023-11-14 19:33:10 -0600

    multiple engine live value changes

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
-    // Simulate data change
-    setInterval(() => {
-        const enginePower = document.querySelector('.engine .power');
-        enginePower.textContent = `${Math.floor(Math.random() * 10000)} kW`;
+    // Function to simulate data change for each engine
+    function simulateDataChange(engineNumber) {
+        const enginePower = document.querySelector(`#engine${engineNumber} .power`);
+        const statusBar = document.querySelector(`#engine${engineNumber} .status-bar`);
         
-        const statusBar = document.querySelector('.status-bar');
+        // Random power value between 0 and 9999 kW
+        enginePower.textContent = `${Math.floor(Math.random() * 10000)} kW`;
+        // Random status bar height
         statusBar.style.height = `${Math.floor(Math.random() * 100)}%`;
+    }
+
+    // Simulate data change for all engines every second
+    setInterval(() => {
+        for (let i = 1; i <= 5; i++) { // Assuming you have 5 engines
+            simulateDataChange(i);
+        }
     }, 1000);
 
-    // Handle switch toggle
+    // Handle switch toggle for all engines
     const switches = document.querySelectorAll('.switch input');
     switches.forEach(sw => {
         sw.addEventListener('change', (event) => {
-            console.log('Switch toggled', event.target.checked);
-            // Here you would handle the switch toggle
+            const engineId = sw.closest('.engine').id;
+            console.log(`${engineId} Switch toggled`, event.target.checked);
+            // Here you can handle the switch toggle for each engine
         });
     });
 });

de87e7c9d25e4e497a72997dc7954b25ce316497
evasquez11 <ethan.f.vasquez-1@ou.edu>
2023-11-14 19:35:06 -0600

    change all occurences from engine to sensor

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Function to simulate data change for each engine
-    function simulateDataChange(engineNumber) {
-        const enginePower = document.querySelector(`#engine${engineNumber} .power`);
-        const statusBar = document.querySelector(`#engine${engineNumber} .status-bar`);
+    // Function to simulate data change for each sensor
+    function simulateDataChange(sensorNumber) {
+        const sensorPower = document.querySelector(`#sensor${sensorNumber} .power`);
+        const statusBar = document.querySelector(`#sensor${sensorNumber} .status-bar`);
         
         // Random power value between 0 and 9999 kW
-        enginePower.textContent = `${Math.floor(Math.random() * 10000)} kW`;
+        sensorPower.textContent = `${Math.floor(Math.random() * 10000)} kW`;
         // Random status bar height
         statusBar.style.height = `${Math.floor(Math.random() * 100)}%`;
     }
 
-    // Simulate data change for all engines every second
+    // Simulate data change for all sensors every second
     setInterval(() => {
-        for (let i = 1; i <= 5; i++) { // Assuming you have 5 engines
+        for (let i = 1; i <= 5; i++) { // Assuming you have 5 sensors
             simulateDataChange(i);
         }
     }, 1000);
 
-    // Handle switch toggle for all engines
+    // Handle switch toggle for all sensors
     const switches = document.querySelectorAll('.switch input');
     switches.forEach(sw => {
         sw.addEventListener('change', (event) => {
-            const engineId = sw.closest('.engine').id;
-            console.log(`${engineId} Switch toggled`, event.target.checked);
-            // Here you can handle the switch toggle for each engine
+            const sensorId = sw.closest('.sensor').id;
+            console.log(`${sensorId} Switch toggled`, event.target.checked);
+            // Here you can handle the switch toggle for each sensor
         });
     });
 });
